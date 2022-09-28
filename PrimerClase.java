08e5c38c33192e52409e9ad3daac0274d8c7102a
Sebaslml98 <114524220+Sebaslml98@users.noreply.github.com>
2022-09-28 15:51:49 -0500

    Update PrimerClase.java

--- PrimerClase.java
+++ PrimerClase.java
@@ -1,6 +1,6 @@
 
 public class PrimerClase {
 	public static void main(String[] parametro) {
-        System.out.println("Hola Mundo Java");
+        System.out.println("Una pequeña edicion a tu codigo");
     }
 }
