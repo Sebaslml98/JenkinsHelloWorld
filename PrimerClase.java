69bf572c5824730f9e4f4aaabb5763a556a9d255
Sebaslml98 <114524220+Sebaslml98@users.noreply.github.com>
2022-09-28 16:25:00 -0500

    Update PrimerClase.java

--- PrimerClase.java
+++ PrimerClase.java
@@ -1,6 +1,6 @@
 
 public class PrimerClase {
 	public static void main(String[] parametro) {
-        System.out.println("Un pequeno cambio a tu codigo con webhook :D");
+        System.out.println("Un pequeno cambio a tu codigo con webhook <8D");
     }
 }
